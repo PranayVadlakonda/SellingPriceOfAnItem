e904e21800f4d6f3b838b040e9f6036f822939f3
pranay <pranayanand61@gmail.com>
2023-05-17 06:00:31 +0530

    print the revised selling price using soutv

--- src/main/java/com/niit/jdp/SellingPriceOfAnITEM.java
+++ src/main/java/com/niit/jdp/SellingPriceOfAnITEM.java
@@ -4,6 +4,7 @@ public class SellingPriceOfAnITEM {
     public static void main(String[] args) {
         String itemName = "kaisabowl";
         double sellingPrice = 10.99;
-
+        double revisedSellingPrice = (10.99*5/100);
+        System.out.println("Revised Selling Price for Rounded Kaisa Bowl is $" + revisedSellingPrice);
     }
 }
