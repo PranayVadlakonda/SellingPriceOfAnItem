824e206814d373ef547ebc6246909cfc7b2d720d
pranay <pranayanand61@gmail.com>
2023-05-17 05:56:26 +0530

    initialize a variable sellingPrice with double  datatype

--- src/main/java/com/niit/jdp/SellingPriceOfAnITEM.java
+++ src/main/java/com/niit/jdp/SellingPriceOfAnITEM.java
@@ -3,7 +3,7 @@ package com.niit.jdp;
 public class SellingPriceOfAnITEM {
     public static void main(String[] args) {
         String itemName = "kaisabowl";
-
+        double sellingPrice = 10.99;
 
     }
 }
